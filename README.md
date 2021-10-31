b60611abfbef42c30d1564a33e030c1abeaf3be8
sing3demons <sing3demons@live.com>
2021-10-31 19:21:05 +0700

    run k6 on docker-compose

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ bind 0.0.0.0
 appendonly yes
 SAVE ""
 ```
+
+
+#### k6 home brew
+```
+brew install k6
+k6 run scripts/test.js
+```
+
+#### k6 on docker-compose
+```
+docker compose run --rm k6 run /scripts/test.js
+```
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+	app.Get("/api", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World")
+	})
+	app.Listen(":8000")
+}
